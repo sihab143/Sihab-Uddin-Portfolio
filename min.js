1901c7e84474e77a101d05e0ae58c4dbd44c6d70
sihab143 <sohanur.sr63@gmail.com>
2022-09-26 20:47:44 +0600

    contact

--- min.js
+++ min.js
@@ -57,3 +57,28 @@ anime.timeline({loop: true})
     document.getElementById(tabname).classList.add('active-tab');
 
   }
+
+
+// navigation
+
+// document.addEventListener("DOMContentLoaded", function(){
+//   window.addEventListener('scroll', function() {
+//       if (window.scrollY > 50) {
+//         document.getElementById('navbar_top').classList.add('fixed-top');
+//         // add padding top to show content behind navbar
+//         navbar_height = document.querySelector('.navbar').offsetHeight;
+//         document.body.style.paddingTop = navbar_height + 'px';
+//       } else {
+//         document.getElementById('navbar_top').classList.remove('fixed-top');
+//          // remove padding top from body
+//         document.body.style.paddingTop = '0';
+//       } 
+//   });
+// }); 
+// DOMContentLoaded  end
+
+
+window.addEventListener('scroll', function(){
+  var header = document.getElementById('nav-header');
+  header.classList.toggle("sticky", window.scrollY > 0);
+})
